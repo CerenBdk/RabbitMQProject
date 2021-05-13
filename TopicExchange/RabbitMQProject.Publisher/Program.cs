82ff11a04769f28425520a1365ad285d56ae7454
CerenBdk <cerenbdk@gmail.com>
2021-05-13 20:01:14 +0300

    RabbitMQ topic exchange example

---
+++ TopicExchange/RabbitMQProject.Publisher/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQProject.Publisher
+{
+    class Program
+    {
+        public enum LogNames
+        {
+            Critical = 1,
+            Error = 2,
+            Warning = 3,
+            Info = 4
+        }
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://tfmwqnnj:SoJ7GgKdKrK18y8BgHMtOytN2iVGixbT@fish.rmq.cloudamqp.com/tfmwqnnj");
+
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                LogNames log1 = (LogNames)new Random().Next(1, 5);
+                LogNames log2 = (LogNames)new Random().Next(1, 5);
+                LogNames log3 = (LogNames)new Random().Next(1, 5);
+
+                string message = $"Log Type:  {log1}-{log2}-{log3}";
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                var routeKey = $"{log1}.{log2}.{log3}";
+
+                channel.BasicPublish("logs-topic", routeKey, null, messageBody);
+
+                Console.WriteLine("Log has been sended: {0}", message);
+
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TopicExchange/RabbitMQProject.Subscriber/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQProject.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://tfmwqnnj:SoJ7GgKdKrK18y8BgHMtOytN2iVGixbT@fish.rmq.cloudamqp.com/tfmwqnnj");
+
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(queueName, "logs-topic", "*.Error.*");
+
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("Log Listening");
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+                Console.WriteLine("Incoming message: " + message);
+
+                //File.AppendAllText("topic-exchanes-routekey.txt", message + "\n");
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+            Console.ReadLine();
+
+        }
+    }
+}
